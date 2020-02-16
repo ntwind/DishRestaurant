d4c50c9384c87bf2d107ef8457939d0030bafc25
dmytro_r <rybak.dr@gmail.com>
2020-02-16 21:48:17 +0200

    prepared API stub with data

--- src/Services/Dishes.js
+++ src/Services/Dishes.js
@@ -2,7 +2,17 @@
 export function getDishes(page, qtu) {
     return new Promise( (resolve, reject) => {
         setTimeout( () => {
-            resolve({a: 1})
+            resolve( new Array(9).fill(
+                    {
+                        title: 'Roasted Butternut Pumkin, Shiitake NMushroom and Haluomi Salad',
+                        descr: 'A hearty mix of fresh greensm, roasted vegetables and golden haluomi makes up this tasty winter salad.',
+                        ingredients: 'meat',
+                        cals: 243,
+                        price: 66,
+                        serves: 2
+                    }
+                )
+            );
         }, 3000);
     })
 }
